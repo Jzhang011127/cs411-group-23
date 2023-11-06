09bf4e233f20d75b0b2f28cebbf391a2b79a2914
Grace Sewell <gsewell@bu.edu>
2023-11-06 16:29:08 -0500

    updated base pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,32 +4,52 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <body>
+    <main>  
+    <div>
+        <Header />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <script src="index.js"></script>
+    </main>
+    </body>
     </>
   )
+}
+function Header() {
+  return (
+    <><div className="header">
+      <div className="logo">
+        <img src="your-logo.png" alt="Logo" width="50" height="50" />
+      </div>
+      <div className="login">
+        <a href="#">Login</a>
+      </div>
+    </div>
+    <div class="holder">
+    <div class="col1">
+    <div class="forecast">
+      <p>
+        forecast
+      </p>
+    </div>
+    <div class="recs-and-weekly">
+      <div class="recs">
+        <p>recs</p>
+      </div>
+      <div class="weekly-forecast">
+        <p>week</p>
+      </div>
+    </div>
+    </div>
+    <div class="col2">
+      <p>alerts</p>
+    </div>
+    </div>
+    </>
+  );
 }
 
 export default App
