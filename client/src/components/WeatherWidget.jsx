89db0194afe019bc3f6a3f28a6a69d6bf4429486
Grace Sewell <gsewell@bu.edu>
2023-11-09 22:10:37 -0500

    fixed weather app

--- client/src/components/WeatherWidget.jsx
+++ client/src/components/WeatherWidget.jsx
@@ -13,25 +13,26 @@ const WeatherWidget = () => {
           const { latitude, longitude } = position.coords;
 
           // Fetch the city and state using Mapbox's Geocoding API
-          const mapboxAccessToken = "YOUR_MAPBOX_ACCESS_TOKEN"; // Replace with your Mapbox access token
+          const mapboxAccessToken = "pk.eyJ1IjoiZ3Nld2VsbDcxMSIsImEiOiJjbG9ydHFxMHYwcnBrMmpsbmJ4czI5bXhxIn0.BBOD7cs2UXRLgyyerJKNFw"; // Replace with your Mapbox access token
           const mapboxGeocodingUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${longitude},${latitude}.json?access_token=${mapboxAccessToken}`;
 
           try {
             const response = await axios.get(mapboxGeocodingUrl);
-            const features = response.data.features;
-            if (features.length > 0) {
-              const place = features[0];
-              const city = place.text;
-              const state = place.context.find((context) => context.id.startsWith("region")).text;
-
+            if (response.data.features.length > 0) {
+              const placeName = response.data.features[0].place_name;
+              const city = placeName.split(", ")[2];
+              const state = placeName.split(", ")[3];
               setLocation(`${city}, ${state}`);
+              //setLocation(placeName);
+            } else {
+              console.error("City not found in the Mapbox response.");
             }
           } catch (error) {
             console.error("Error fetching location", error);
           }
 
           // Fetch weather data using the OpenWeatherMap API
-          const openWeatherMapApiKey = "YOUR_OPENWEATHERMAP_API_KEY"; // Replace with your OpenWeatherMap API key
+          const openWeatherMapApiKey = "263c364c0c876d4542c6dc220af45a4a"; // Replace with your OpenWeatherMap API key
           const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${openWeatherMapApiKey}&units=metric`;
 
           try {
@@ -49,8 +50,9 @@ const WeatherWidget = () => {
     } else {
       console.error("Geolocation is not supported in this browser.");
     }
+    console.log("Weather Data:", weather.weather[0].description);
   }, []);
-
+  const source = `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`
   
   return (
     <div className="weather-widget">
@@ -58,9 +60,30 @@ const WeatherWidget = () => {
         <p>Loading...</p>
       ) : (
         <>
-          <p className="city">{location}</p>
-          <p>Temperature: {weather.main.temp}°C</p>
-          <p>Weather: {weather.weather[0].description.charAt(0).toUpperCase() + weather.weather[0].description.slice(1)}</p>
+        <div className="Test">
+          <div className="location">
+              <img className="images" src="../64113.png" width="15px"></img>
+              {location}
+          </div>
+          <div className="desc">
+              {weather.weather[0].description.charAt(0).toUpperCase() + weather.weather[0].description.slice(1)}
+              <br></br>
+              <br></br>
+              
+              <p><img src={source}></img></p>
+          </div>
+        </div>
+        <div>
+        <div className="temperature">
+          <p></p>    
+          <br></br>  
+          <br></br>    
+          <p>{Math.round(32 + 1.8 * (weather.main.temp))}°F</p>
+        </div>
+        <div className="weathericon">
+          
+        </div>
+        </div>
         </>
       )}
     </div>
