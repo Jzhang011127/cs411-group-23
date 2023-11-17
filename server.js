65a60f92b39be79975207696fa6aa287f87da96e
Ahmad Sadiq <asadiq@bu.edu>
2023-11-16 20:29:01 -0500

    made new beanh

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ const app = express();
 
 app.get('/', (req, res) => res.send('Server running'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4550;
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
