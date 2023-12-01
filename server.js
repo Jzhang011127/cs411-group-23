cdc0ff5d8c6d9ec10599e817380f2054773b7441
Tanay Damani <tanaydamani@Tanays-MacBook-Air.local>
2023-12-01 17:55:24 -0500

    changed server so it connects to mongodb for user dignup

--- server.js
+++ server.js
@@ -1,12 +1,41 @@
+import mongoose from 'mongoose';
 import express from 'express';
 import dotenv from 'dotenv';
 
 dotenv.config({ path: './config/config.env' });
 
+const db = 'mongodb+srv://tdamani:darshdamani@test.ehu5btu.mongodb.net/?retryWrites=true&w=majority';
+
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('MongoDB Connected...'))
+  .catch((err) => console.log(err));
+  export default mongoose;
 const app = express();
+app.use(express.json());
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+  email: String,
+});
+
+const User = mongoose.model('User', userSchema);
+
+app.post('/signup', async (req, res) => {
+  const { username, password, email } = req.body;
+
+  const user = new User({ username, password, email });
+  await user.save();
+
+  res.status(200).json({ status: 'User created' });
+});
 
 app.get('/', (req, res) => res.send('Server running'));
 
-const PORT = process.env.PORT || 4550;
+const PORT = process.env.PORT || 5500;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)); n
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
